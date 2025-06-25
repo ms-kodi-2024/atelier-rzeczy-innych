2084ccd5b3bb089665cdaaf5bcf3f78ec723bd69
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-06-25 19:45:48 +0200

    Add production entry point and support for serving frontend from backend

---
+++ src/main.prod.ts
@@ -0,0 +1,36 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import { join } from 'path';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
+    },
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  const clientBuildPath = join(process.cwd(), 'client', 'build');
+  expressApp.use(express.static(clientBuildPath));
+  expressApp.get('*', (_, res) => {
+    res.sendFile(join(clientBuildPath, 'index.html'));
+  });
+
+  const port = process.env.PORT || 5000;
+  await app.listen(port);
+}
+bootstrap();
